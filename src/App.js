c9e549b9f67bb4b8343bed259054817c30108f01
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-02-12 17:47:24 +0900

    Routes 구성하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './components/MainPage';
+import TechPage from './components/TechPage';
+import BlogPage from './components/BlogPage';
+
 
 function App() {
+  // root url : main page component
+  // tech url : tech page component
+  // blog url : blog page component
   return (
-    <div>app</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} element={<MainPage />}></Route>
+        <Route path={'/tech'} element={<TechPage />}></Route>
+        <Route path={'/blog'} element={<BlogPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/TechPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TechPage() {
+    return (
+        <div>
+            <h1>TechPage</h1>
+        </div>
+    )
+}
