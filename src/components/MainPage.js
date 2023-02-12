425b455661da7f360365e7e4805a824aa3b3025b
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-02-12 17:52:34 +0900

    Update MainPage.js

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function MainPage() {
     return (
         <div>
             <h1>MainPage</h1>
+            <Link to='/blog'>Blog</Link> | <Link to='/tech'>Tech</Link>
+            {/* Link와 a태그의 차이는 Link는 새로운 페이지를 불러오는 것 이 아님 */}
         </div>
     )
 }
