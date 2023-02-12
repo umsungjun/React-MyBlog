b17cb0cd9eed773d432b7efe594c4393fcd2f7cf
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-02-12 18:14:57 +0900

    Nested Routes 구성하기

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage';
 import TechPage from './components/TechPage';
 import BlogPage from './components/BlogPage';
-
+import JavascriptPage from './components/JavascriptPage'
+import ReactPage from './components/ReactPage';
 
 function App() {
   // root url : main page component
@@ -13,8 +14,11 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path={'/'} element={<MainPage />}></Route>
-        <Route path={'/tech'} element={<TechPage />}></Route>
-        <Route path={'/blog'} element={<BlogPage />}></Route>
+        <Route path={'tech'} element={<TechPage />}>
+          <Route path='javascript' element={<JavascriptPage />} />
+          <Route path='react' element={<ReactPage />} />
+        </Route>
+        <Route path={'blog'} element={<BlogPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/components/TechPage.js
+++ src/components/TechPage.js
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import { Outlet, Link } from 'react-router-dom'
 export default function TechPage() {
     return (
-        <div>
-            <h1>TechPage</h1>
-        </div>
+        <>
+            <div>
+                <h1>TechPage</h1>
+                <Link to='javascript'>Javascript</Link> | <Link to='react'>React</Link>
+            </div>
+            <Outlet />
+        </>
     )
 }
