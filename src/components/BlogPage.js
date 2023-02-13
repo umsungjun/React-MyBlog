2d83b0ad3bde5dc9c51fe56e428b507174aa0a25
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-02-13 23:02:26 +0900

    update

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -2,12 +2,13 @@ import React, { useContext } from 'react'
 import UserContext from '../store/user'
 export default function BlogPage() {
     const dispatch = UserContext(useContext)
-    console.log(dispatch.props)
+    console.log(dispatch.props.value.user.name)
+    console.log(dispatch.props.value.user.job)
     return (
         <div>
             <h1>BlogPage</h1>
             <button onClick={() => {
-                dispatch.props.value({ type: 'changeJob', text: 'BE-developer' })
+                dispatch.props.value.dispatch({ type: 'changeJob', text: 'BE-developer' })
             }}>changeJob</button>
         </div>
     )
--- src/store/user.js
+++ src/store/user.js
@@ -24,7 +24,7 @@ export default function UserStore(props) {
 
     console.log(user)
     return (
-        <UserContext.Provider value={dispatch}>
+        <UserContext.Provider value={{ user, dispatch }}>
             {props.children}
         </UserContext.Provider>
     )
